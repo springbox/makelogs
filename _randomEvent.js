b4d1055fb1b785bdb68cbde812bf9309f7062f8c
Ben Balentine <ben.balentine@springbox.com>
2017-08-01 16:43:13 -0500

    initial commit

--- _randomEvent.js
+++ _randomEvent.js
@@ -25,8 +25,6 @@ var dayMoment = argv.start.clone();
 var day;
 
 module.exports = function RandomEvent(indexPrefix) {
-  var event = {};
-
   var i = ++eventCounter;
   var iInDay = i % countPerDay;
 
@@ -61,87 +59,8 @@ module.exports = function RandomEvent(indexPrefix) {
   var date = new Date(day.year, day.month, day.date, hours, minutes, seconds, ms);
   var dateAsIso = date.toISOString();
 
-  switch (indexInterval) {
-    case 'yearly':
-      event.index = indexPrefix + dateAsIso.substr(0, 4);
-      break;
-
-    case 'monthly':
-      event.index = indexPrefix + dateAsIso.substr(0, 4) + '.' + dateAsIso.substr(5, 2);
-      break;
-    case 'daily':
-      event.index = indexPrefix + dateAsIso.substr(0, 4) + '.' + dateAsIso.substr(5, 2) + '.' + dateAsIso.substr(8, 2);
-      break;
-
-    default:
-      event.index = indexPrefix + Math.floor(i / indexInterval);
-      break;
-  }
-
-  event['@timestamp'] = dateAsIso;
-  event.ip = samples.ips();
-  event.extension = samples.extensions();
-  event.response = samples.responseCodes();
-
-  event.geo = {
-    coordinates: samples.airports(),
-    src: samples.countries(),
-    dest: samples.countries()
+  return {
+      "controllerType": "testType",
+      "originDate": dateAsIso
   };
-  event.geo.srcdest = event.geo.src + ':' + event.geo.dest;
-
-  event['@tags'] = [
-    samples.tags(),
-    samples.tags2()
-  ];
-  event.utc_time = dateAsIso;
-  event.referer = 'http://' + samples.referrers() + '/' + samples.tags() + '/' + samples.astronauts();
-  event.agent = samples.userAgents();
-  event.clientip = event.ip;
-  event.bytes = event.response < 500 ? samples.lessRandomRespSize(event.extension) : 0;
-
-  switch (event.extension) {
-  case 'php':
-    event.host = 'theacademyofperformingartsandscience.org';
-    event.request = '/people/type:astronauts/name:' + samples.astronauts() + '/profile';
-    event.phpmemory = event.memory = event.bytes * 40;
-    break;
-  case 'gif':
-    event.host = 'motion-media.theacademyofperformingartsandscience.org';
-    event.request = '/canhaz/' + samples.astronauts() + '.' + event.extension;
-    break;
-  case 'css':
-    event.host = 'cdn.theacademyofperformingartsandscience.org';
-    event.request = '/styles/' + samples.stylesheets();
-    break;
-  default:
-    event.host = 'media-for-the-masses.theacademyofperformingartsandscience.org';
-    event.request = '/uploads/' + samples.astronauts() + '.' + event.extension;
-    break;
-  }
-
-  event.url = 'https://' + event.host + event.request;
-
-  event['@message'] = event.ip + ' - - [' + dateAsIso + '] "GET ' + event.request + ' HTTP/1.1" ' +
-      event.response + ' ' + event.bytes + ' "-" "' + event.agent + '"';
-  event.spaces = 'this   is   a   thing    with lots of     spaces       wwwwoooooo';
-  event.xss = '<script>console.log("xss")</script>';
-  event.headings = [
-    '<h3>' + samples.astronauts() + '</h5>',
-    'http://' + samples.referrers() + '/' + samples.tags() + '/' + samples.astronauts()
-  ];
-  event.links = [
-    samples.astronauts() + '@' + samples.referrers(),
-    'http://' + samples.referrers() + '/' + samples.tags2() + '/' + samples.astronauts(),
-    'www.' + samples.referrers()
-  ];
-
-  event.relatedContent = samples.relatedContent();
-
-  event.machine = {
-    os: samples.randomOs(),
-    ram: samples.randomRam()
-  };
-
-  return event;
 };
